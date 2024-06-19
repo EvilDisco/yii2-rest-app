a7d2016ef3e4eb68b93816da568f927ee920eec3
Eugene Yarosh <yarosh@webant.ru>
2024-06-19 17:51:57 +0800

    Фикс типа поля

--- common/services/article/dtos/ArticleAuthorDto.php
+++ common/services/article/dtos/ArticleAuthorDto.php
@@ -7,7 +7,7 @@ class ArticleAuthorDto
     public function __construct(
         public int $id,
         public string $full_name,
-        public string $birth_year,
+        public int $birth_year,
         public string $bio
     ) {}
 }
